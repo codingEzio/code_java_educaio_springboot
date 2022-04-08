439bcc94ccd63c29e93f51efe5fa2bcca3f3d857
CodingEzio <assassinste@gmail.com>
2022-04-09 07:52:41 +0800

    Changing the filter to CollaborativeFilter

---
+++ src/main/java/com/homer/spring/basics/movierecommendersystem/lesson02/CollaborativeFilter.java
@@ -0,0 +1,9 @@
+package com.homer.spring.basics.movierecommendersystem.lesson02;
+
+public class CollaborativeFilter {
+	public String[] getRecommendations(String movie) {
+		// Implement logic of collaborative filter
+
+		return new String[] {};
+	}
+}
--- src/main/java/com/homer/spring/basics/movierecommendersystem/lesson02/RecommenderImplementation.java
+++ src/main/java/com/homer/spring/basics/movierecommendersystem/lesson02/RecommenderImplementation.java
@@ -2,7 +2,7 @@ package com.homer.spring.basics.movierecommendersystem.lesson02;
 
 public class RecommenderImplementation {
 	public String[] recommendMovies(String movie) {
-		ContentBasedFilter filter = new ContentBasedFilter();
+		CollaborativeFilter filter = new CollaborativeFilter();
 
 		return filter.getRecommendations("test");
 	}
