9387611bed557d797db9ad7a3ffc0440904d4523
CodingEzio <assassinste@gmail.com>
2022-04-09 06:47:49 +0800

    Implementation code (currently using hardcoded result)

---
+++ src/main/java/com/homer/spring/basics/movierecommendersystem/lesson01/ContentBasedFilter.java
@@ -0,0 +1,7 @@
+package com.homer.spring.basics.movierecommendersystem.lesson01;
+
+public class ContentBasedFilter {
+	public String[] getRecommendations(String movie) {
+		return new String[] {"Interstellar", "Solaris", "Gravity"};
+	}
+}
---
+++ src/main/java/com/homer/spring/basics/movierecommendersystem/lesson01/RecommenderImplementation.java
@@ -0,0 +1,7 @@
+package com.homer.spring.basics.movierecommendersystem.lesson01;
+
+public class RecommenderImplementation {
+	public String[] recommendMovies(String movie) {
+		return new String[] {"M1", "M2", "M3"};
+	}
+}
