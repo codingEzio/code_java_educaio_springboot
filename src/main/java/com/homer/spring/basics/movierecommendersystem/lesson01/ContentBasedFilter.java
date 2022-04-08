0be5ae4abbd84f47313092f4881ada8b6eaac75f
CodingEzio <assassinste@gmail.com>
2022-04-09 07:02:26 +0800

    Demonstrating the whole recommending process

--- src/main/java/com/homer/spring/basics/movierecommendersystem/lesson01/ContentBasedFilter.java
+++ src/main/java/com/homer/spring/basics/movierecommendersystem/lesson01/ContentBasedFilter.java
@@ -2,6 +2,8 @@ package com.homer.spring.basics.movierecommendersystem.lesson01;
 
 public class ContentBasedFilter {
 	public String[] getRecommendations(String movie) {
+		// Implement logic of content based filter
+
 		return new String[] {"Interstellar", "Solaris", "Gravity"};
 	}
 }
--- src/main/java/com/homer/spring/basics/movierecommendersystem/lesson01/RecommenderImplementation.java
+++ src/main/java/com/homer/spring/basics/movierecommendersystem/lesson01/RecommenderImplementation.java
@@ -2,6 +2,8 @@ package com.homer.spring.basics.movierecommendersystem.lesson01;
 
 public class RecommenderImplementation {
 	public String[] recommendMovies(String movie) {
-		return new String[] {"M1", "M2", "M3"};
+		ContentBasedFilter filter = new ContentBasedFilter();
+
+		return filter.getRecommendations("test");
 	}
 }
