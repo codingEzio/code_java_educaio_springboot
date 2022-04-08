42d1fa842ba27e6b3cecb7925d82a479e92ded2b
CodingEzio <assassinste@gmail.com>
2022-04-09 06:37:55 +0800

    Initialize files for lesson 01

---
+++ src/main/java/com/homer/spring/basics/movierecommendersystem/lesson01/MovieRecommenderSystemApplication.java
@@ -0,0 +1,13 @@
+package com.homer.spring.basics.movierecommendersystem.lesson01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieRecommenderSystemApplication {
+
+	public static void main (String[] args) {
+		SpringApplication.run(MovieRecommenderSystemApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/homer/spring/basics/movierecommendersystem/lesson01/README.md
@@ -0,0 +1,4 @@
+
+### Instructions
+1. Right-click to create a new *package* named *lesson01*
+2. Copy the `.java` file into this folder (add `.lesson01` to the package name)
