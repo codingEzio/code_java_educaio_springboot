92fbc656bc23b255ec017ca5268b91e4fb0a48c5
CodingEzio <assassinste@gmail.com>
2022-04-09 08:16:38 +0800

    Removed unnecessay line feed for Filter interface

--- src/main/java/com/homer/spring/basics/movierecommendersystem/lesson02/Filter.java
+++ src/main/java/com/homer/spring/basics/movierecommendersystem/lesson02/Filter.java
@@ -1,6 +1,5 @@
 package com.homer.spring.basics.movierecommendersystem.lesson02;
 
 public interface Filter {
-	public String[]
-	getRecommendations(String movie);
+	public String[] getRecommendations(String movie);
 }
