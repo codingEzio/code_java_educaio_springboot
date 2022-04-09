00cf6caf0a47527b58d49cac007c6953a0c09cc3
CodingEzio <assassinste@gmail.com>
2022-04-09 10:46:13 +0800

    Explain an annotation called @SpringBootApplication

--- src/main/java/com/homer/spring/basics/movierecommendersystem/lesson03/MovieRecommenderSystemApplication.java
+++ src/main/java/com/homer/spring/basics/movierecommendersystem/lesson03/MovieRecommenderSystemApplication.java
@@ -5,6 +5,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.util.Arrays;
 
+// Other than using @Component to mark Beans, @Autowired to mark dependencies, we would
+// also need @ComponentScan to tell where those Beans are (so Spring could find them).
+// @ComponentScan tells Spring to scan a specific package and all of its sub-packages.
+//
+// The @SpringBootApplication is equivalent to the three annotations
+//  @Configuration              declares a class as the source for bean definitions
+//  @EnableAutoConfiguration    allows the application to add beans using classpath definitions
+//  @ComponentScan ⭐️⭐️⭐️       directs Spring to search for components in the path specified
 @SpringBootApplication
 public class MovieRecommenderSystemApplication {
 
