abdf2184db7f62716071ea8a129cbda8d4f90b47
CodingEzio <assassinste@gmail.com>
2022-04-12 05:39:23 +0800

    Mark CollaborativeFilter as the main one to use

--- src/main/java/com/homer/spring/basics/movierecommendersystem/lesson04/CollaborativeFilter.java
+++ src/main/java/com/homer/spring/basics/movierecommendersystem/lesson04/CollaborativeFilter.java
@@ -1,9 +1,14 @@
 package com.homer.spring.basics.movierecommendersystem.lesson04;
 
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+@Component
+@Primary
 public class CollaborativeFilter implements Filter {
 	public String[] getRecommendations(String movie) {
 		// Implement logic of collaborative filter
-
-		return new String[] {};
+		
+		return new String[] {"Interstellar", "Solaris", "Gravity"};
 	}
 }
